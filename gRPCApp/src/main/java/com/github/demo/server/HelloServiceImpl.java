8785476e2801fcb8083f4c9b6e372433137699c8
swapnil.gaikwad <swapnil.gaikwad@flipkart.com>
2021-01-25 08:29:58 +0530

    Add setup

---
+++ gRPCApp/src/main/java/com/github/demo/server/HelloServiceImpl.java
@@ -0,0 +1,25 @@
+package com.github.demo.server;
+
+
+import com.github.grpc.HelloRequest;
+import com.github.grpc.HelloResponse;
+import com.github.grpc.HelloServiceGrpc;
+import io.grpc.stub.StreamObserver;
+
+public class HelloServiceImpl extends HelloServiceGrpc.HelloServiceImplBase {
+    @Override
+    public void hello(HelloRequest request, StreamObserver<HelloResponse> responseObserver) {
+        String greeting = new StringBuilder()
+                .append("Hello, ")
+                .append(request.getFirstName())
+                .append(" ")
+                .append(request.getLastName())
+                .toString();
+        HelloResponse response = HelloResponse.newBuilder()
+                .setGreeting(greeting)
+                .build();
+
+        responseObserver.onNext(response);
+        responseObserver.onCompleted();
+    }
+}
